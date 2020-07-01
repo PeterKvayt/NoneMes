bf46eec80d81fd35f07c0e0f2e0ca4891411c4e3
PeterKvayt <peter.kvayt@gmail.com>
2020-07-01 17:47:19 +0300

    Add view models.

---
+++ src/NoMessage/ViewModels/UserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoMessage.ViewModels
+{
+    public sealed class UserViewModel
+    {
+        [Required]
+        public string Firstname { get; set; }
+        [Required]
+        public string Lastname { get; set; }
+        [Required]
+        public string Patronomic { get; set; }
+        public string Address { get; set; }
+        public DateTime DayOfBith { get; set; }
+    }
+}
