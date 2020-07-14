0325c0814ecb30933d27676c43e2398754d9b1a9
PeterKvayt <peter.kvayt@gmail.com>
2020-07-14 13:53:40 +0300

    Add input-text component.

---
+++ src/AngularApp/ClientApp/src/app/components/input-text/input-text.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
+import { InputText } from './InputText';
+
+@Component({
+  selector: 'app-input-text',
+  templateUrl: './input-text.component.html',
+  styleUrls: []
+})
+export class InputTextComponent implements OnInit {
+
+  @Input() item: InputText;
+
+  @Output() valueChange = new EventEmitter<InputText>();
+
+  public onValueChange(value: string): void {
+      this.item.value = value;
+      this.valueChange.emit(this.item);
+  }
+
+  public ngOnInit(): void {
+    this.item.valid = this.item.valid === undefined ? true : this.item.valid;
+  }
+
+}
