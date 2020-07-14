396e0822cc10a0fcbcbb1838a3aeca2dd1467afa
PeterKvayt <peter.kvayt@gmail.com>
2020-07-14 13:27:50 +0300

    Alter layout.

--- src/AngularApp/ClientApp/src/app/app.module.ts
+++ src/AngularApp/ClientApp/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
