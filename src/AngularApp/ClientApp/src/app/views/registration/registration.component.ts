c9654853d347868a8a03c70a8dc7f2336439ec67
PeterKvayt <peter.kvayt@gmail.com>
2020-07-16 15:38:35 +0300

    Add UserRegisterService.

--- src/AngularApp/ClientApp/src/app/views/registration/registration.component.ts
+++ src/AngularApp/ClientApp/src/app/views/registration/registration.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { InputText } from 'src/app/component-elements/InputText';
+import { UserRegisterService } from 'src/app/services/UserRegisterService';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.css'],
+  providers: [UserRegisterService]
 })
 export class RegistrationComponent implements OnInit {
+
+  constructor(service: UserRegisterService) {
+
+  }
 
   public emailInput: InputText = {
     label: 'Email',
@@ -27,6 +33,13 @@ export class RegistrationComponent implements OnInit {
     label: 'Repit password',
     errorText: 'Not match.'
   };
+
+  public onRegisterClick(): void {
+    console.log(this.emailInput.value);
+    console.log(this.loginInput.value);
+    console.log(this.passwordInput.value);
+    console.log(this.repitPasswordInput.value);
+  }
 
   ngOnInit() {
   }
