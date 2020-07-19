37dec92a6dbb48944b81d679df6a1524c630c91f
PeterKvayt <peter.kvayt@gmail.com>
2020-07-19 20:21:25 +0300

    Alter AccountService.

--- src/AngularApp/ClientApp/src/app/services/AccountService.ts
+++ src/AngularApp/ClientApp/src/app/services/AccountService.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { RegisterUserModel } from '../models/User';
+import { RegisterUserModel } from '../models/RegisterUserModel';
 
 @Injectable()
 export class AccountService {
 
   constructor(private httpClient: HttpClient) { }
 
-  private host = 'https://localhost:44347/api/accounts/register';
+  private host = 'https://localhost:44347/api/accounts/';
 
   public register(model: RegisterUserModel) {
     const headers = { 'content-type': 'application/json'};
-    return this.httpClient.post(this.host, model, {headers});
+    return this.httpClient.post(this.host + 'register', model, {headers});
   }
 
   public signIn(model: RegisterUserModel) {
     const headers = { 'content-type': 'application/json'};
-    return this.httpClient.post(this.host, model, {headers});
+    return this.httpClient.post(this.host + 'login', model, {headers});
   }
 }
