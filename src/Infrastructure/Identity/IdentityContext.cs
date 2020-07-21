081fd3d39cbb5a06d6ac59a4e8879f9cccbdaa90
PeterKvayt <peter.kvayt@gmail.com>
2020-07-21 23:11:36 +0300

    Modify IdentityContext.

--- src/Infrastructure/Identity/IdentityContext.cs
+++ src/Infrastructure/Identity/IdentityContext.cs
@@ -1,10 +1,11 @@
 ﻿using Core.Identity;
+using Core.Interfaces;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Identity
 {
-    public class IdentityContext : IdentityDbContext<ApplicationUser>
+    public class IdentityContext : IdentityDbContext<ApplicationUser>, IIdentityRepository
     {
         public IdentityContext(DbContextOptions<IdentityContext> options)
            : base(options)
